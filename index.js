ae06a5c1b182cb03ba21f81270b747e7e8924471
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-06-05 00:12:04 +0600

    verify token create and verify user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -32,12 +33,49 @@ async function run() {
     const surveyCollections = client.db("survey").collection("surveyList");
     const paymentCollections = client.db("survey").collection("payments");
 
-    app.get("/user", async (req, res) => {
-      const users = await userCollections.find({}).toArray();
-      res.send(users);
+
+    // jwt token create
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "20h",
+      });
+      res.send({ token });
     });
 
-    app.post("/user", async (req, res) => {
+    // middleware 
+
+    const verifyToken = (req, res, next) => {
+      //console.log('insert the token',req.headers)
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorize access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorize access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // verify admin middleware
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollections.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    // admin operation in backend
+
+    // get admin user profile
+    app.post("/user", verifyToken, async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
       const isExist = await userCollections.findOne(query);
@@ -45,6 +83,90 @@ async function run() {
       const result = await userCollections.insertOne(user);
       res.send(result);
     });
+
+
+    app.get("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollections.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
+    
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      //console.log(req.headers.authorization);
+      const role = req.query.role;
+      let query = {};
+      if (role) {
+        query = { role: role };
+      }
+      const users = await userCollections.find(query).toArray();
+      res.send(users);
+    })
+
+    app.delete("/user/:id",verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    // payment intent
+    app.post("/create_payment_intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.post("/payment", verifyToken, async (req, res) => {
+      const payment = req.body;
+      const userEmail = payment.email
+      const user = await userCollections.findOne({email : userEmail})
+      if(user){
+        const updateDoc = {
+          $set : {
+            role : 'pro-user'
+          }
+        }
+        const userUpdate = await userCollections.updateOne({email : userEmail}, updateDoc)
+        res.send(userUpdate)
+        
+      }
+
+    });
+
+    app.get('/payment', async (req, res) => {
+      const payments = await paymentCollections.find().toArray();
+      res.send(payments);
+    })
+
+    app.put('/updateUserRole', verifyToken, async (req, res) => {
+      const userInfo = req.body;
+      const id = userInfo._id;
+      if(id){
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: userInfo.role,
+          },
+        };
+        const result = await userCollections.updateOne(filter, updateDoc);
+        res.send(result);
+      };
+    })
+
+    
 
     app.get("/survey", async (req, res) => {
       const email = req.query.email;
@@ -56,7 +178,7 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/survey", async (req, res) => {
+    app.post("/survey", verifyToken, async (req, res) => {
       const information = req.body;
       const PresentDate = new Date().getDate();
       const month = new Date().getMonth() + 1;
@@ -82,14 +204,14 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/survey/:id", async (req, res) => {
+    app.delete("/survey/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await surveyCollections.deleteOne(query);
       res.send(result);
     });
 
-    app.patch("/survey/:id", async (req, res) => {
+    app.patch("/survey/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const survey = req.body;
       const filter = { _id: new ObjectId(id) };
@@ -106,78 +228,11 @@ async function run() {
       res.send(result);
     });
 
-    // payment intent
-    app.post("/create_payment_intent", async (req, res) => {
-      const { price } = req.body;
-      const amount = parseInt(price * 100);
+    
+    
+    
 
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: "usd",
-        payment_method_types: ["card"],
-      });
-      res.send({
-        clientSecret: paymentIntent.client_secret,
-      });
-    });
-
-    app.post("/payment", async (req, res) => {
-      const payment = req.body;
-      const userEmail = payment.email
-      const user = await userCollections.findOne({email : userEmail})
-      if(user){
-        const updateDoc = {
-          $set : {
-            role : 'pro-user'
-          }
-        }
-        const result = await userCollections.updateOne({email : userEmail}, updateDoc)
-      }
-
-      const result = await paymentCollections.insertOne(payment);
-      res.send(result);
-    });
-
-    app.get('/payment', async (req, res) => {
-      const payments = await paymentCollections.find().toArray();
-      res.send(payments);
-    })
-
-    app.get('/users', async (req, res) => {
-      const role = req.query.role;
-      let query = {};
-      if (role) {
-        query = { role: role };
-      }
-      const users = await userCollections.find(query).toArray();
-      res.send(users);
-    })
-    app.put('/updateUserRole', async (req, res) => {
-      const userInfo = req.body;
-      const id = userInfo._id;
-      if(id){
-        const filter = { _id: new ObjectId(id) };
-        const updateDoc = {
-          $set: {
-            role: userInfo.role,
-          },
-        };
-        const result = await userCollections.updateOne(filter, updateDoc);
-        res.send(result);
-      };
-    })
-
-    app.get("/user/admin/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email };
-      const user = await userCollections.findOne(query);
-      let admin = false;
-      if (user) {
-        admin = user?.role === "admin";
-      }
-      res.send({ admin });
-    });
-
+    // get surveyor user profile
     app.get("/user/surveyor/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
@@ -189,6 +244,8 @@ async function run() {
       res.send({ surveyor });
     });
 
+
+    // get proUser user profile
     app.get("/user/proUser/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
@@ -200,12 +257,7 @@ async function run() {
       res.send({ proUser });
     });
 
-    app.delete("/user/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await userCollections.deleteOne(query);
-      res.send(result);
-    });
+    
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
